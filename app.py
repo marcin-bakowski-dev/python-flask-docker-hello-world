20db50a6f7e4c17e661352dbaf326b14855deb07
Marcin Bąkowski <marcin-bakowski-dev@users.noreply.github.com>
2018-02-14 16:28:02 +0100

    add build docker image bash script

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
+from subprocess import check_output
+
 from flask import Flask
 from version import VERSION
 
 app = Flask(__name__)
+hostname = check_output('hostname').strip().decode("utf-8")
 
 
 @app.route("/")
 def hello():
-    return "Flask inside Docker!! %s" % VERSION
+    return "[%s] Flask inside Docker!! %s" % (hostname, VERSION)
 
 
 if __name__ == "__main__":
--- build-docker-image.sh
+++ build-docker-image.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
 VERSION=$(python version.py)
-docker build -t simple-flask-app:${VERSION} .
+IMAGE='simple-flask-app'
+
+docker build -t ${IMAGE}:${VERSION} .
+
+if [ "$1" == "push" ]
+then
+    if [ -z "${DOCKER_ID_USER}" ]
+    then
+        echo -e "DOCKER_ID_USER env not set.\nexport DOCKER_ID_USER=your_docker_hub_id;\n"
+        exit -1
+    fi
+    docker tag ${IMAGE}:${VERSION} ${DOCKER_ID_USER}/${IMAGE}:${VERSION}
+    docker push ${DOCKER_ID_USER}/${IMAGE}:${VERSION}
+fi
 
